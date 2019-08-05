7a3ecc31e92de450ea889a7f0c124af5e285922a
spabba <sunilpabba@gmail.com>
2019-08-05 14:29:37 -0500

    added new dirs

---
+++ demo-8/variables.tf
@@ -0,0 +1,24 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "terraform.pem"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "terraform.pub"
+}
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-0cfee17793b08a293"
+    us-west-2 = "ami-0b37e9efc396e4c38"
+    eu-west-1 = "ami-03746875d916becc0"
+  }
+}
